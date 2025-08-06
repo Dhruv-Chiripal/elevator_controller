19d22db4084bbf7b5b3f4290c18d655eb4e5638e
Dhruv Chiripal <147789444+Dhruv-Chiripal@users.noreply.github.com>
2025-08-06 22:05:11 +0530

    Update tb_elevator_controller.v

--- tb_elevator_controller.v
+++ tb_elevator_controller.v
@@ -32,8 +32,7 @@ module tb_elevator_controller;
 
     // Test sequence
     initial begin
-        $monitor("Time=%0t | Floor: %d | Moving: %b | Door: %b | Dir: %b | Reqs: %b",
-                 $time, floor, moving, door, direction, dut.requests);
+        $monitor("Time=%0t | Floor: %d | Moving: %b | Door: %b | Dir: %b | Reqs: %b",$time, floor, moving, door, direction, dut.requests);
 
         // Reset
         reset = 1; req = 0;
@@ -41,26 +40,34 @@ module tb_elevator_controller;
 
         // Test Case 1: 0 → 2 → 1
         $display("\n--- TC1 ---");
-        req = 4'b0100; #10; req = 0; #200;
-        req = 4'b0010; #10; req = 0; #200;
+        req = 4'b0100; #10; 
+        req = 0; #200;
+        req = 4'b0010; #10; 
+        req = 0; #200;
 
         // Test Case 2: 0 → 2, 3
         $display("\n--- TC2 ---");
-        req = 4'b1100; #10; req = 0; #300;
+        req = 4'b1100; #10; 
+        req = 0; #300;
 
         // Test Case 3: 3 → 1, 0
         $display("\n--- TC3 ---");
-        req = 4'b0011; #10; req = 0; #300;
+        req = 4'b0011; #10; 
+        req = 0; #300;
 
         // Test Case 4: UP priority from floor 1
         $display("\n--- TC4 ---");
-        req = 4'b0010; #10; req = 0; #200;
-        req = 4'b0101; #10; req = 0; #400;
+        req = 4'b0010; #10; 
+        req = 0; #200;
+        req = 4'b0101; #10; 
+        req = 0; #400;
 
         // Test Case 5: Opposite request while going up
         $display("\n--- TC5 ---");
-        req = 4'b1000; #10; req = 0; #25;
-        req = 4'b0001; #10; req = 0; #500;
+        req = 4'b1000; #10; 
+        req = 0; #25;
+        req = 4'b0001; #10; 
+        req = 0; #500;
 
         $display("\n--- DONE ---");
         $finish;
